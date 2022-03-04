e244181f4917d6c21d06ed526269894e9708a189
dguglielmi-git <dguglielmi.asm@gmail.com>
2022-03-04 11:40:40 -0300

    Refactor OrderDetails

--- components/Orders/Order/content/OrderDetails.js
+++ components/Orders/Order/content/OrderDetails.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Divider } from "semantic-ui-react";
-import OrderStatusLabel from "./OrderDetailContent/OrderStatusLabel";
-import ProgressOrder from "./OrderDetailContent/ProgressOrder";
-import OrderDetailedTable from "./OrderDetailContent/OrderDetailedTable";
-import OrderCommentSection from "./OrderDetailContent/OrderCommentSection";
-import ButtonBack from "./OrderDetailContent/ButtonBack";
-import { markMessageAsRead } from "../../../../api/orderMessage";
-import { verifyUserType } from "../../../../utils/util";
 import { useTranslation } from "react-i18next";
 import useMsgs from "../../../../hooks/useMsgs";
+import { verifyUserType } from "../../../../utils/util";
+import ButtonBack from "./OrderDetailContent/ButtonBack";
+import ProgressOrder from "./OrderDetailContent/ProgressOrder";
+import OrderStatusLabel from "./OrderDetailContent/OrderStatusLabel";
+import OrderDetailedTable from "./OrderDetailContent/OrderDetailedTable";
+import OrderCommentSection from "./OrderDetailContent/OrderCommentSection";
+import { markMessageAsRead } from "../../../../api/orderMessage";
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.css';
 import "primereact/resources/primereact.min.css";
@@ -38,7 +38,11 @@ export default function OrderDetails(props) {
                 goBack={ goBack }
                 label={ t('orderDetailsBackButton') }
             />
-            <OrderStatusLabel order={ order } />
+
+            <OrderStatusLabel
+                order={ order }
+            />
+
             <ProgressOrder
                 order={ order }
                 userType={ userType }
@@ -46,8 +50,13 @@ export default function OrderDetails(props) {
                 orderBlocked={ orderBlocked }
                 setOrderBlocked={ setOrderBlocked }
             />
-            <OrderDetailedTable order={ order } />
+
+            <OrderDetailedTable
+                order={ order }
+            />
+
             <Divider section />
+
             <OrderCommentSection
                 order={ order }
                 setReloadOrder={ setReloadOrder }
