5f1fc7ef9c80e2f8db2746f4b0d8fadca225767a
dguglielmi-git <dguglielmi.asm@gmail.com>
2022-03-04 12:20:56 -0300

    Refactor InfoProducto

--- components/Producto/InfoProducto/InfoProducto.js
+++ components/Producto/InfoProducto/InfoProducto.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { size } from "lodash";
+import "moment/locale/es";
 import moment from "moment";
+import { size } from "lodash";
 import { useTranslation } from "react-i18next";
 import CarouselScreenshots from "../CarouselScreenshots";
-import "moment/locale/es";
-import "../../../locales/i18n";
-
 
 export default function InfoProducto(props) {
     const { producto } = props;
