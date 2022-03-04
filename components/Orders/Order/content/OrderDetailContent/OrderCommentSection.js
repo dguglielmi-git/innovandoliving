81086339694ff29055bd3381164a4247693010ba
dguglielmi-git <dguglielmi.asm@gmail.com>
2022-03-04 11:47:53 -0300

    Refactor OrderCommentSection

--- components/Orders/Order/content/OrderDetailContent/OrderCommentSection.js
+++ components/Orders/Order/content/OrderDetailContent/OrderCommentSection.js
@@ -3,17 +3,22 @@ import { Container } from "semantic-ui-react";
 import Comments from "../Comments";
 
 export default function OrderCommentSection(props) {
-    const { order, setReloadOrder, sendLabel, userType, orderBlocked } = props;
+    const {
+        order,
+        setReloadOrder,
+        sendLabel,
+        userType,
+        orderBlocked } = props;
 
     return (
         <div className="order-detail__mainbox-comments">
             <Container>
                 <Comments
                     order={ order }
-                    setReloadOrder={ setReloadOrder }
-                    sendLabel={ sendLabel }
                     userType={ userType }
+                    sendLabel={ sendLabel }
                     orderBlocked={ orderBlocked }
+                    setReloadOrder={ setReloadOrder }
                 />
             </Container>
         </div>
