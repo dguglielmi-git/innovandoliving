a79b556ecb457b7a7e70cf6547e7c054ce3040e7
dguglielmi-git <dguglielmi.asm@gmail.com>
2022-03-04 18:50:05 -0300

    Refactor of platform

--- pages/productos/[platform].js
+++ pages/productos/[platform].js
@@ -39,7 +39,6 @@ export default function Platform() {
             const response = await getTotalProductosPlatform(query.platform);
             setTotalProductos(response);
         })()
-
     }, [query]);
 
     return (
