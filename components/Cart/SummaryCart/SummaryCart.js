972b9bc43bfcb5fa0208878c7745281ddc430653
dguglielmi-git <dguglielmi.asm@gmail.com>
2022-03-04 11:07:13 -0300

    Refactor SummaryCart

--- components/Cart/SummaryCart/SummaryCart.js
+++ components/Cart/SummaryCart/SummaryCart.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { forEach, size } from "lodash";
 import useCart from "../../../hooks/useCart";
-import SubtitleSummaryCart from "./sections/SubtitleSummaryCart"
-import ProductsListOnCart from "./sections/ProductsListOnCart";
-import TotalPriceOfProductsList from './sections/TotalPriceOfProductsList';
-import ContinuePurchaseButton from './sections/ContinuePurchaseButton';
 import { getDiscountPrice } from '../../../utils/util';
+import ProductsListOnCart from "./sections/ProductsListOnCart";
+import SubtitleSummaryCart from "./sections/SubtitleSummaryCart"
+import ContinuePurchaseButton from './sections/ContinuePurchaseButton';
+import TotalPriceOfProductsList from './sections/TotalPriceOfProductsList';
 
 export default function SummaryCart(props) {
     const {
@@ -16,11 +16,9 @@ export default function SummaryCart(props) {
         totalPrice,
         setTotalPrice
     } = props;
-    const [loading, setLoading] = useState(true);
     const { removeProductCart } = useCart();
 
     useEffect(() => {
-        if (size(products) >= 0) setLoading(false)
         let price = 0;
         (async () => {
             await forEach(products, (product) => {
