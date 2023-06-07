8cf6f32d657f3d98677e1835bac99bfd71bcb03c
danyguglielmi <daniel.guglielmi@globant.com>
2023-06-07 19:06:18 -0300

    refactor(fe19): modify the property requested for the address id, changed .id by ._id

--- components/Account/ListAddress/ListAddressGrid.js
+++ components/Account/ListAddress/ListAddressGrid.js
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { map } from "lodash";
 import { Grid } from "semantic-ui-react";
 import Address from "./Address";
 import {
-    MOBILE_COL_SIZE,
-    TABLET_COL_SIZE,
-    COMPUTER_COL_SIZE
+  MOBILE_COL_SIZE,
+  TABLET_COL_SIZE,
+  COMPUTER_COL_SIZE,
 } from "../../../utils/constants";
-import BasicLoading from "../../BasicLoading/BasicLoading";
 
 export default function ListAddressGrid(props) {
+  const {
+    t,
+    addresses,
+    selectEnable,
+    logout,
+    setReloadAddresses,
+    openModal,
+    idSelected,
+    setIdSelected,
+  } = props;
 
-    const {
-        t,
-        addresses,
-        selectEnable,
-        logout,
-        setReloadAddresses,
-        openModal,
-        idSelected,
-        setIdSelected
-    } = props;
-
-    return (
-        <Grid>
-            <div>
-                { map(addresses, (address) => (
-                    <Grid.Column
-                        key={ address.id }
-                        mobile={ MOBILE_COL_SIZE }
-                        tablet={ TABLET_COL_SIZE }
-                        computer={ COMPUTER_COL_SIZE }>
-                        <Address
-                            selectEnable={ selectEnable }
-                            address={ address }
-                            logout={ logout }
-                            setReloadAddresses={ setReloadAddresses }
-                            openModal={ openModal }
-                            idSelected={ idSelected }
-                            setIdSelected={ setIdSelected }
-                            t={ t }
-                        />
-                    </Grid.Column>
-                )) }
-            </div>
-        </Grid>
-    )
+  return (
+    <Grid>
+      <div>
+        {map(addresses, (address) => (
+          <Grid.Column
+            key={address._id}
+            mobile={MOBILE_COL_SIZE}
+            tablet={TABLET_COL_SIZE}
+            computer={COMPUTER_COL_SIZE}
+          >
+            <Address
+              selectEnable={selectEnable}
+              address={address}
+              logout={logout}
+              setReloadAddresses={setReloadAddresses}
+              openModal={openModal}
+              idSelected={idSelected}
+              setIdSelected={setIdSelected}
+              t={t}
+            />
+          </Grid.Column>
+        ))}
+      </div>
+    </Grid>
+  );
 }
