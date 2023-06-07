f807a04fe757257e2ac46a82940c18b88f00b601
danyguglielmi <daniel.guglielmi@globant.com>
2023-06-07 19:13:44 -0300

    refactor(fe19): add logout function as a second parameter to the getAddressbyId call since it is required by the new logic to get an address properly with the new service

--- components/Cart/ConfirmCart/ConfirmCart.js
+++ components/Cart/ConfirmCart/ConfirmCart.js
@@ -1,90 +1,96 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { forEach } from "lodash";
-import { Divider } from 'primereact/divider';
-import ButtonBack from './sections/ButtonBack';
-import ListItemsCart from './sections/ListItemsCart';
-import ButtonContinue from './sections/ButtonContinue';
-import HeaderTotalCart from './sections/HeaderTotalCart';
-import FooterTotalCart from './sections/FooterTotalCart';
+import { Divider } from "primereact/divider";
+import ButtonBack from "./sections/ButtonBack";
+import ListItemsCart from "./sections/ListItemsCart";
+import ButtonContinue from "./sections/ButtonContinue";
+import HeaderTotalCart from "./sections/HeaderTotalCart";
+import FooterTotalCart from "./sections/FooterTotalCart";
 import {
-    DELIVERY_OPTION_DELIVERY,
-    DELIVERY_OPTION_EXTERNAL_PROVIDER
-} from '../../../utils/constants';
-import { getAddressById } from '../../../api/address';
-import { calcShippingDelivery, getDiscountPrice } from '../../../utils/util';
+  DELIVERY_OPTION_DELIVERY,
+  DELIVERY_OPTION_EXTERNAL_PROVIDER,
+  ERROR_GETTING_ADDRESS_DETAILS,
+} from "../../../utils/constants";
+import useAuth from "../../../hooks/useAuth";
+import { getAddressById } from "../../../api/address";
+import { calcShippingDelivery, getDiscountPrice } from "../../../utils/util";
 import { getConfigurations } from "../../../api/configurations";
 
 export default function ConfirmCart(props) {
-    const {
-        t,
-        setStep,
-        products,
-        address,
-        deliveryOption,
-        shippingPrice,
-        setShippingPrice } = props;
-    const [totalPrice, setTotalPrice] = useState(0);
-    const [loading, setLoading] = useState(false);
+  const {
+    t,
+    setStep,
+    products,
+    address,
+    deliveryOption,
+    shippingPrice,
+    setShippingPrice,
+  } = props;
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const { logout } = useAuth();
 
-    useEffect(async () => {
-        setLoading(true);
-        const configs = await getConfigurations();
+  useEffect(async () => {
+    setLoading(true);
+    const configs = await getConfigurations();
 
-        if (address) {
-            const clientAddress = await getAddressById(address);
-            try {
-                switch (deliveryOption) {
-                    case DELIVERY_OPTION_DELIVERY:
-                        setShippingPrice(calcShippingDelivery(configs, clientAddress));
-                        break;
-                    case DELIVERY_OPTION_EXTERNAL_PROVIDER:
-                        setShippingPrice(3200);
-                        break;
-                    default:
-                        setShippingPrice(0);
-                }
-
-            } catch (error) {
-                console.log("An Error happened when trying to get the Address details: " + error)
-            }
+    if (address) {
+      const clientAddress = await getAddressById(address, logout);
+      try {
+        switch (deliveryOption) {
+          case DELIVERY_OPTION_DELIVERY:
+            setShippingPrice(calcShippingDelivery(configs, clientAddress));
+            break;
+          case DELIVERY_OPTION_EXTERNAL_PROVIDER:
+            setShippingPrice(3200);
+            break;
+          default:
+            setShippingPrice(0);
         }
-        setLoading(false);
+      } catch (error) {
+        console.error(`${ERROR_GETTING_ADDRESS_DETAILS}  ${error}`);
+      }
+    }
+    setLoading(false);
+  }, []);
 
-    }, []);
+  useEffect(() => {
+    let price = 0;
+    (async () => {
+      await forEach(products, (product) => {
+        if (product.producto.discount) {
+          let discountPrice = getDiscountPrice(
+            parseFloat(product.producto.price),
+            product.producto.discount
+          );
+          price += parseFloat(discountPrice) * parseFloat(product.quantity);
+        } else {
+          price +=
+            parseFloat(product.producto.price) * parseFloat(product.quantity);
+        }
+      });
+    })();
+    setTotalPrice(price);
+  }, [products]);
 
-    useEffect(() => {
-        let price = 0;
-        (async () => {
-            await forEach(products, (product) => {
-                if (product.producto.discount) {
-                    let discountPrice = getDiscountPrice(parseFloat(product.producto.price), product.producto.discount);
-                    price += parseFloat(discountPrice) * parseFloat(product.quantity);
-                } else {
-                    price += (parseFloat(product.producto.price) * parseFloat(product.quantity));
-                }
-            });
-        })()
-        setTotalPrice(price);
-    }, [products]);
+  return (
+    <div className="confirm-cart">
+      <ButtonBack setStep={setStep} />
 
-    return (
-        <div className="confirm-cart">
-            <ButtonBack setStep={ setStep } />
+      <HeaderTotalCart />
 
-            <HeaderTotalCart />
+      <ListItemsCart products={products} />
 
-            <ListItemsCart products={ products } />
+      <FooterTotalCart
+        t={t}
+        deliveryOption={deliveryOption}
+        totalPrice={totalPrice}
+        shippingPrice={shippingPrice}
+      />
 
-            <FooterTotalCart
-                t={ t }
-                deliveryOption={ deliveryOption }
-                totalPrice={ totalPrice }
-                shippingPrice={ shippingPrice }
-            />
+      <Divider align="center" />
 
-            <Divider align="center" />
-
-            <ButtonContinue setStep={ setStep } loading={ loading } />
-        </div>
-    )
+      <ButtonContinue setStep={setStep} loading={loading} />
+    </div>
+  );
 }
