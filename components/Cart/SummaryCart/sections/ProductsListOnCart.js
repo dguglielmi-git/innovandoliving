6e31442c28a294f5552b5f39715a06d1567b0fda
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-08 06:38:17 -0300

    refactor(fe19): add unique key to the map method that is rendering the Summary details in the Product List of the cart

--- components/Cart/SummaryCart/sections/ProductsListOnCart.js
+++ components/Cart/SummaryCart/sections/ProductsListOnCart.js
@@ -5,36 +5,36 @@ import { useTranslation } from "react-i18next";
 import BasicLoading from "../../../BasicLoading/BasicLoading";
 
 export default function ProductsListOnCart(props) {
-    const {
-        products,
-        setReloadCart,
-        removeProductCart } = props;
-    const [loading, setLoading] = useState(true);
-    const { t } = useTranslation();
+  const { products, setReloadCart, removeProductCart } = props;
+  const [loading, setLoading] = useState(true);
+  const { t } = useTranslation();
 
-    useEffect(() => {
-        if (products) setLoading(false);
-    }, [products]);
+  useEffect(() => {
+    if (products) setLoading(false);
+  }, [products]);
 
-    const EmptyCart = () => (
-        <div className="empty-cart">
-            { loading && <h5>{ <BasicLoading label={ t('cartSummaryLoadingCart') } /> }</h5> }
-            { !loading && <h5>{ t('cartSummaryEmptyCart') }</h5> }
-        </div>
-    )
+  const EmptyCart = () => (
+    <div className="empty-cart">
+      {loading && (
+        <h5>{<BasicLoading label={t("cartSummaryLoadingCart")} />}</h5>
+      )}
+      {!loading && <h5>{t("cartSummaryEmptyCart")}</h5>}
+    </div>
+  );
 
-    return (
-        <div className="datascroller-cart">
-            <div className="card">
-                { map(products, (data) => (
-                    <SummaryDetail
-                        data={ data }
-                        setReloadCart={ setReloadCart }
-                        removeProductCart={ removeProductCart }
-                    />
-                )) }
-                { (size(products) === 0) && <EmptyCart /> }
-            </div>
-        </div>
-    )
+  return (
+    <div className="datascroller-cart">
+      <div className="card">
+        {map(products, (data, index) => (
+          <SummaryDetail
+            key={index}
+            data={data}
+            setReloadCart={setReloadCart}
+            removeProductCart={removeProductCart}
+          />
+        ))}
+        {size(products) === 0 && <EmptyCart />}
+      </div>
+    </div>
+  );
 }
