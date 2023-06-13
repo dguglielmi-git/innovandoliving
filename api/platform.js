1ea7fc735cfd6b2527ed0ae674304309ec16abda
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-13 18:33:28 -0300

    refactor(fe36): remove the code that sends the token for validation since this endpoint is public and no validation is required

--- api/platform.js
+++ api/platform.js
@@ -1,18 +1,11 @@
-import { FILTER_SORTED_PLATFORMS, TOKEN_IS_MISSING } from "../utils/constants";
+import { FILTER_SORTED_PLATFORMS } from "../utils/constants";
 import { fetchRetryParams } from "../utils/fetch";
 import { getBackendURL } from "../utils/util";
-import { getToken } from "./token";
 
 export async function getSortedPlatforms() {
   try {
-    const token = getToken();
-    if (!token) {
-      console.error(`getSortedPlatforms: ${TOKEN_IS_MISSING}`);
-      return [];
-    }
     const params = {
       headers: {
-        "x-token": token,
         "Content-Type": "application/json",
       },
     };
