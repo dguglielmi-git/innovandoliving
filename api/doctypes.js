efc2e7fc208e64f63f9d4e0c421618d002c5e9f3
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-14 07:03:00 -0300

    refactor(fe42): adapt the call to get doc types from the backend to use the new cloud function backend instead of Strapi for retrieving the doc type list

--- api/doctypes.js
+++ api/doctypes.js
@@ -1,11 +1,34 @@
-import { fetchRetry } from "../utils/fetch";
+import { getBackendURL } from "../utils/util";
+import { getToken } from "./token";
+
+// export async function getDocTypes() {
+//   try {
+//     const url = `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/doc-types`;
+//     const response = await fetchRetry(url);
+//     const result = await response.json();
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
+// }
 
 export async function getDocTypes() {
   try {
-    const url = `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/doc-types`;
-    const response = await fetchRetry(url);
-    const result = await response.json();
-    return result;
+    const token = getToken();
+    if (!token) {
+      return null;
+    }
+
+    const url = `${getBackendURL()}/doctypes`;
+    const params = {
+      headers: {
+        "x-token": token,
+        "Content-Type": "application/json",
+      },
+    };
+    const result = await fetchRetryParams(url, params);
+    return await response.json();
   } catch (error) {
     console.log(error);
     return null;
