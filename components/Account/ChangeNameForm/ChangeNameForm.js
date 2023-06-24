30a9c7937febbafdf8c0bdfd0a35d86d67f51b8d
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-23 21:37:24 -0300

    refactor(fe41): refactor the way we update the user name by calling the new and generic function updateUser()

--- components/Account/ChangeNameForm/ChangeNameForm.js
+++ components/Account/ChangeNameForm/ChangeNameForm.js
@@ -1,72 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 import { useTranslation } from "react-i18next";
-import { updateNameApi } from "../../../api/user";
+import { updateUser } from "../../../api/user";
 import "../../../locales/i18n";
 
 export default function ChangeNameForm(props) {
-    const { user, logout, setReloadUser } = props;
-    const [loading, setLoading] = useState(false);
-    const { t } = useTranslation();
+  const { user, logout, setReloadUser } = props;
+  const [loading, setLoading] = useState(false);
+  const { t } = useTranslation();
 
-    const formik = useFormik({
-        initialValues: initialValues(user.name, user.lastname),
-        validateOnChange: false,
-        validationSchema: Yup.object(validationSchema()),
-        onSubmit: async (formData) => {
-            setLoading(true);
-            const response = await updateNameApi(user.id, formData, logout);
-            if (!response) {
-                toast.error(t('accountChangeNameFormErrorUpdate'));
-            } else {
-                toast.success(t('accountChangeNameFormOkUpdate'));
-                setReloadUser(true);
-            }
-            setLoading(false);
-        }
-    });
+  const formik = useFormik({
+    initialValues: initialValues(user.name, user.lastname),
+    validateOnChange: false,
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      setLoading(true);
+      const response = await updateUser(formData, logout);
+      if (!response) {
+        toast.error(t("accountChangeNameFormErrorUpdate"));
+      } else {
+        toast.success(t("accountChangeNameFormOkUpdate"));
+        setReloadUser(true);
+      }
+      setLoading(false);
+    },
+  });
 
-    return (
-        <div className="change-name-form">
-            <h4>{ t('accountChangeNameFormChangeLabel') }</h4>
-            <Form onSubmit={ formik.handleSubmit }>
-                <Form.Group widths="equal">
-                    <Form.Input
-                        name="name"
-                        placeholder={ t('accountChangeNameFormNewName') }
-                        onChange={ formik.handleChange }
-                        value={ formik.values.name }
-                        error={ formik.errors.name }
-                    />
-                    <Form.Input
-                        name="lastname"
-                        placeholder={ t('accountChangeNameFormNewLastname') }
-                        onChange={ formik.handleChange }
-                        value={ formik.values.lastname }
-                        error={ formik.errors.lastname }
-                    />
-                </Form.Group>
-                <Button className="submit" loading={ loading }>
-                    { t('accountChangeNameFormButtonUpdate') }
-                </Button>
-            </Form>
-        </div>
-    )
+  return (
+    <div className="change-name-form">
+      <h4>{t("accountChangeNameFormChangeLabel")}</h4>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group widths="equal">
+          <Form.Input
+            name="name"
+            placeholder={t("accountChangeNameFormNewName")}
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            error={formik.errors.name}
+          />
+          <Form.Input
+            name="lastname"
+            placeholder={t("accountChangeNameFormNewLastname")}
+            onChange={formik.handleChange}
+            value={formik.values.lastname}
+            error={formik.errors.lastname}
+          />
+        </Form.Group>
+        <Button className="submit" loading={loading}>
+          {t("accountChangeNameFormButtonUpdate")}
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 function initialValues(name, lastname) {
-    return {
-        name: name || "",
-        lastname: lastname || "",
-    }
+  return {
+    name: name || "",
+    lastname: lastname || "",
+  };
 }
 
 function validationSchema() {
-    return {
-        name: Yup.string().required(true),
-        lastname: Yup.string().required(true),
-    }
+  return {
+    name: Yup.string().required(true),
+    lastname: Yup.string().required(true),
+  };
 }
