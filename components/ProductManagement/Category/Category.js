d6524a8713fd9a6b3ca70f3a4ec72202dfa18c54
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-08-17 00:37:37 -0300

    feat(fe52): new category component for managing the CRUD of categories

--- components/ProductManagement/Category/Category.js
+++ components/ProductManagement/Category/Category.js
@@ -1,32 +1,31 @@
-import React, {useState} from 'react'
-import { ListBox } from 'primereact/listbox';
-import { Menubar } from 'primereact/menubar';
-
+import React, { useState } from 'react'
+import { ListBox } from 'primereact/listbox'
+import { Menubar } from 'primereact/menubar'
 
 export default function Category() {
-    const [selectedCity, setSelectedCity] = useState(null);
-    const cities = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
-    ];
+  const [selectedCity, setSelectedCity] = useState(null)
+  const cities = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' },
+  ]
 
-    const items = [
-        {
-            label: 'Add',
-            icon: 'pi pi-fw pi-plus-circle'
-        },
-        {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil'
-        },
-        {
-            label: 'Remove',
-            icon: 'pi pi-fw pi-trash'
-        }
-    ];
+  const items = [
+    {
+      label: 'Add',
+      icon: 'pi pi-fw pi-plus-circle',
+    },
+    {
+      label: 'Edit',
+      icon: 'pi pi-fw pi-pencil',
+    },
+    {
+      label: 'Remove',
+      icon: 'pi pi-fw pi-trash',
+    },
+  ]
 
   return (
     <div className="category-management">
@@ -34,9 +33,16 @@ export default function Category() {
         <h2>Categories</h2>
       </div>
       <div className="category-management__list">
-      <Menubar model={items} />
-      <div className="card flex justify-content-center">  
-            <ListBox filter value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name" className="w-full md:w-14rem" />
+        <Menubar model={items} />
+        <div className="card flex justify-content-center">
+          <ListBox
+            filter
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.value)}
+            options={cities}
+            optionLabel="name"
+            className="w-full md:w-14rem"
+          />
         </div>
       </div>
     </div>
