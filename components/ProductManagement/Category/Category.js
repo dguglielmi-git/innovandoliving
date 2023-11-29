91d29b8e7c0d2d53376b6a3a85107edb55a08fbf
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-11-29 18:28:52 -0300

    feat(fe60): adding table structure for categories

--- components/ProductManagement/Category/Category.js
+++ components/ProductManagement/Category/Category.js
@@ -7,6 +7,18 @@ export default function Category () {
         <div className='category-management__mainbox__title'>
           <h2 className='poppins-600-regular'>Categories</h2>
         </div>
+        <div className='category-management__mainbox__list'>
+          <div className='category-management__mainbox__list__headers'>
+            <div className='category-management__mainbox__list__headers__label'>
+              <h2 className='poppins-600-regular'>Category Title</h2>
+            </div>
+            <div className='category-management__mainbox__list__headers__label'>
+              <h2 className='poppins-600-regular'>Options</h2>
+            </div>
+          </div>
+          <div className='category-management__mainbox__list__rows'></div>
+          <div className='category-management__mainbox__list__actions'></div>
+        </div>
       </div>
     </div>
   )
